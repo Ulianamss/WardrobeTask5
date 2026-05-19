7ac90faffbb8ea3b218ad23be8628bec6b487e92
Ulianamss <ulianamss@gmail.com>
2026-05-19 17:58:34 +0300

    fixed suspense and layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,31 @@
 import { Outlet } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../store/store';
 import { Navbar } from './Navbar';
 import {Notification} from './Notification';
+import { Suspense } from 'react';
 import styles from '../App.module.css';
+
+function LoadingSpinner() {
+  return <div className={styles.loading}>Загрузка...</div>;
+}
 
 export function Layout() {
   return (
-    <div className={styles.app}>
-      <Navbar />
-      <Notification />
-      <main className={styles.main}>
-        <Outlet />
-      </main>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className={styles.app}>
+          <Navbar />
+          <Notification />
+
+          <main className={styles.main}>
+            <Suspense fallback={<LoadingSpinner />}>
+              <Outlet />
+            </Suspense>
+          </main>
+        </div>
+      </PersistGate>
+    </Provider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store/store';
 import { App } from './App';
-
 import './styles/global.css';
-import styles from './styles/Loading.module.css';
-
-function LoadingSpinner() {
-  return (
-    <div className={styles.loadingContainer}>
-      <div className={styles.spinner}></div>
-      <p>Загрузка...</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Suspense fallback={<LoadingSpinner />}>
-        <App />
-      </Suspense>
-    </PersistGate>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
